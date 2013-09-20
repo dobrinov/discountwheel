6fbdbd3a543c4ed3322d377a0e007457dbd878c3
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-21 01:38:41 +0300

    Generate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Discountwheel::Application.config.secret_key_base = 'e9ae37088abf3ec535f7cce1f7a1add012f6a8402ff69fe111a4f66b67be74e3c07222de6ccde53a3dd831660baf5262d21cca9b64cf17031bea8b9d4c436036'
