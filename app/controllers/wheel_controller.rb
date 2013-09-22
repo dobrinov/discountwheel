94c775c30d448c653403c9b179009031caf647c3
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-23 00:37:34 +0300

    Calculate the rotation in the rails app

--- app/controllers/wheel_controller.rb
+++ app/controllers/wheel_controller.rb
@@ -3,4 +3,12 @@ class WheelController < ApplicationController
   def index
   end
 
+  def spin
+    @power = rand(360)
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Discountwheel::Application.routes.draw do
 
-  get 'wheel', to: 'wheel#index'
+  get 'wheel',      to: 'wheel#index'
+  get 'wheel/spin', to: 'wheel#spin'
+
+  root to: 'wheel#index'
+
 end
