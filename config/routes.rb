f0617351a041591b40ebb10b7084e05175cabccf
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-23 00:40:57 +0300

    Add POST root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Discountwheel::Application.routes.draw do
   get 'wheel',      to: 'wheel#index'
   get 'wheel/spin', to: 'wheel#spin'
 
-  root to: 'wheel#index'
+  post '/', to: 'wheel#index'
 
 end
