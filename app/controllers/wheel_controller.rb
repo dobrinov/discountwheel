241a1e96284563b0df1efa5ed1f4e78cdac4be08
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 01:53:47 +0300

    Fix JS syntax error

--- app/controllers/wheel_controller.rb
+++ app/controllers/wheel_controller.rb
@@ -2,8 +2,10 @@ class WheelController < ApplicationController
 
   def index
     @app_id   = '371914089608290'
-    @auth_url = 'https://discountwheel.herokuapp.com/'
-    @scope    = 'email' 
+    @canvas_url = 'https://discountwheel.herokuapp.com/'
+    @scope    = 'email'
+
+    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{Base64.encode64(@canvas_url)}&scope=#{@scope}"
   end
 
   def spin
