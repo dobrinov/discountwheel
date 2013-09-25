eae9ca44820978b26648b71fadfe820dd3a84d3d
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 02:29:25 +0300

    Add root get path and update authentication condition

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Discountwheel::Application.routes.draw do
   get 'wheel',      to: 'wheel#index'
   get 'wheel/spin', to: 'wheel#spin'
 
+  get  '/', to: 'wheel#index'
   post '/', to: 'wheel#index'
 
 end
