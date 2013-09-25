8230ef6cbcff2759bf3891af3e622bb879be505b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 02:43:08 +0300

    Add app_id prefix

--- app/controllers/wheel_controller.rb
+++ app/controllers/wheel_controller.rb
@@ -3,7 +3,7 @@ class WheelController < ApplicationController
   def index
     @app_id     = '371914089608290'
     @page_name  = 'Dobrinovs-lab'
-    @canvas_url = URI.escape("https://www.facebook.com/#{@page_name}/#{@app_id}")
+    @canvas_url = URI.escape("https://www.facebook.com/#{@page_name}/app_#{@app_id}")
     @scope      = 'email'
 
     @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{@canvas_url}&scope=#{@scope}"
