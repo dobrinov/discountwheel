2c17e65773cfbfb43891f24c46624722dac08cb2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 02:08:54 +0300

    Change X-Frame option

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Discountwheel
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOW-FROM https://*.facebook.com'
+      'X-Frame-Options' => 'SAMEORIGIN'
     }
   end
 end
