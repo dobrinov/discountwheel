c3dd11c62f0b20ae81e1787aa8bbdb9a37d69bd6
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 00:10:52 +0300

    Add method for decoding base64 strings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  helper_method :base64_url_decode
+
+   def base64_url_decode(str)
+     str += '=' * (4 - str.length.modulo(4))
+     Base64.decode64(str.tr('-_','+/'))
+   end
 end
