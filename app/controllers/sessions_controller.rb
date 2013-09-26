616542ea3ae8afabf3dab54cc00c1ed6511216f1
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 22:11:51 +0300

    Redirect to page canvas

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,25 @@
 class SessionsController < ApplicationController
 
+  before_filter :init_instance_variables
+
   def create
+  end
+
+  def index
+  end
+
+  private
+
+  def init_instance_variables
     @app_id     = '371914089608290'
     @page_name  = 'Dobrinovs-lab'
     @page_id    = '747088781984697'
 
-    # https://developers.facebook.com/docs/reference/dialogs/oauth/
-    # https://developers.facebook.com/docs/appsonfacebook/pagetabs/
-    @canvas_url = URI.escape("https://discountwheel.herokuapp.com/callback")
-    @scope      = 'email'
+    @callback_url        = URI.escape("https://discountwheel.herokuapp.com/callback")
+    @page_tab_canvas_url = "https://www.facebook.com/pages/#{@page_name}/#{@page_id}?v=app_#{@app_id}"
+    @scope               = 'email'
 
-    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{@canvas_url}&scope=#{@scope}"
-  end
-
-  def index
+    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{@callback_url}&scope=#{@scope}"
   end
 
 end
