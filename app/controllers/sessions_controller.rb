4a8aacb09f0e95835643829490cc2ae2357bf698
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 22:17:46 +0300

    Fix page canvas url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     @page_id    = '747088781984697'
 
     @callback_url        = URI.escape("https://discountwheel.herokuapp.com/callback")
-    @page_tab_canvas_url = "https://www.facebook.com/pages/#{@page_name}/#{@page_id}?v=app_#{@app_id}"
+    @page_tab_canvas_url = "https://www.facebook.com/pages/#{@page_name}/#{@page_id}?id=#{@page_id}&v=app_#{@app_id}"
     @scope               = 'email'
 
     @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{@callback_url}&scope=#{@scope}"
