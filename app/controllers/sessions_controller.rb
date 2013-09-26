4fe6c449d3c776996843bb83085803ed9a3c4032
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 21:37:25 +0300

    Do the same with the views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def create
+    @app_id     = '371914089608290'
+    @page_name  = 'Dobrinovs-lab'
+    @page_id    = '747088781984697'
+
+    # https://developers.facebook.com/docs/reference/dialogs/oauth/
+    # https://developers.facebook.com/docs/appsonfacebook/pagetabs/
+    @canvas_url = URI.escape("https://discountwheel.herokuapp.com/callback")
+    @scope      = 'email'
+
+    @auth_url = "https://www.facebook.com/dialog/oauth?client_id=#{@app_id}&redirect_uri=#{@canvas_url}&scope=#{@scope}"
+  end
+
+  def index
+    raise params.inspect
+  end
+
+end
