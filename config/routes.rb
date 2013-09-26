6bcc47fb04811359ec259596a3154e657d299082
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-26 22:22:05 +0300

    Remove wheel prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Discountwheel::Application.routes.draw do
 
-  get 'wheel',      to: 'wheel#index'
-  get 'wheel/spin', to: 'wheel#spin'
+  get  '/',         to: 'wheel#index'
+  get  '/spin',     to: 'wheel#spin'
 
   get  '/callback', to: 'sessions#index'
   post '/',         to: 'sessions#create'
