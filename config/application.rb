0f4a7ea1a271021d7c7a24ea20865eadd89f60cf
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-27 17:12:17 +0300

    Overwrite some additiona headers

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module Discountwheel
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOW-FROM https://apps.facebook.com'
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff'
     }
   end
 end
