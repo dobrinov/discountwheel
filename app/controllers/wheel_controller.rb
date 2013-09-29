7f3e0a943795d8f9990793eaff8b72e9cba162ba
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-09-29 22:19:36 +0300

    Add restriction for games count

--- app/controllers/wheel_controller.rb
+++ app/controllers/wheel_controller.rb
@@ -1,6 +1,6 @@
 class WheelController < ApplicationController
 
-  helper_method :prize_for, :text_for
+  helper_method :prize_for, :text_for, :code_for
 
   before_filter :init_instance_variables
 
@@ -8,11 +8,17 @@ class WheelController < ApplicationController
     @likes_page = user_likes_page?(current_user, @page_id)
   end
 
+  before_filter :verify_allowed_to_spin, except: [:already_played]
+
   def index
   end
 
   def spin
     @angle = 2 * 360 + ((rand(360) / 15).floor * 15)
+    @prize = prize_for(@angle)
+
+    current_user.discount_wheel_spins << DiscountWheelSpin.new({ prize: @prize })
+    current_user.save
 
     respond_to do |format|
       format.js
@@ -23,6 +29,10 @@ class WheelController < ApplicationController
   end
 
   private
+
+  def verify_allowed_to_spin
+    redirect_to wheel_already_played_path if current_user.discount_wheel_spins.any?
+  end
 
   def prize_for(angle)
     wheel_sectors = [ 20, 15, 25, 30, 20, 15, 100, 15, 20, 30, 25, 15, 20, 15, 20, 25, 20, 15, 25, 15, 20, 25, 20, 15 ]
@@ -35,6 +45,14 @@ class WheelController < ApplicationController
       'безплатна дреха'
     else
       "#{prize}% отстъпка"
+    end
+  end
+
+  def code_for(prize)
+    if(prize == 100)
+      current_user.name + ' ' + current_user.surname
+    else
+      "#{rand(999)}#{prize - 4}"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Discountwheel::Application.routes.draw do
 
-  get  '/wheel/spin', to: 'wheel#spin'
-  get  '/callback',   to: 'sessions#index'
+  get  '/wheel/spin',           to: 'wheel#spin'
+  get  '/wheel/already_played', to: 'wheel#already_played'
+  get  '/callback',             to: 'sessions#index'
 
-  get  '/wheel',      to: 'wheel#index'
-  post '/',           to: 'sessions#create'
+  get  '/wheel',                to: 'wheel#index'
+  post '/',                     to: 'sessions#create'
 
 end
