3e064bdc2f502c624d1bd119720d46f9f590877b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-01-17 01:23:31 +0200

    Add terms and conditions page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Discountwheel::Application.routes.draw do
   get  '/callback',             to: 'sessions#index'
 
   get  '/wheel',                to: 'wheel#index'
+  get  '/terms',                to: 'wheel#terms'
   post '/',                     to: 'sessions#create'
 
   get '/spins/reset',          to: 'spins#reset'
